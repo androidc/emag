ed61e0600e45f26ccbd6485c612097f3da53a2be
androidc <chizztectep@gmail.com>
2023-07-04 00:13:31 +0300

    basket

---
+++ EMag/EMag/App/Entity/Basket.swift
@@ -0,0 +1,12 @@
+//Created by chizztectep on 03.07.2023
+
+import Foundation
+
+
+struct AddBasketResult: Codable {
+    let result: Int
+}
+
+struct CommonBasketResult: Codable {
+    let result: Int
+}
