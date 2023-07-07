7e313c4bf6cc8ae9bb72b2c6970303eb1d276dc0
androidc <chizztectep@gmail.com>
2023-07-07 22:09:39 +0300

    lesson7

---
+++ EMag/EMag/App/Flows/RegisterView.swift
@@ -0,0 +1,100 @@
+//Created by chizztectep on 07.07.2023 
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    // MARK: - Vars
+    var dismissAction: (() -> Void)
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var email: String = ""
+    @State private var creditCard: String = ""
+    @State private var bio: String = "bio:"
+    @State private var textStyle = UIFont.TextStyle.body
+    @State private var selectedGender = "Male"
+    let genders = ["Male", "Female"]
+    
+    // MARK: - Functions
+
+    
+//    func register() {
+//
+//    }
+    
+    // MARK: - body
+    var body: some View {
+        NavigationView {
+                   Form {
+                    
+                       Section {
+                          
+                              // Text(verbatim: "Login")
+                               //TextView(text: $username, textStyle: $textStyle)
+                                 //  .padding(10)
+                           
+                           TextField("UserName", text: $username).padding(10)
+                           SecureField("Password", text: $password).padding(10)
+                           TextField("Email", text: $email).padding(10)
+                           Picker("Gender",selection: $selectedGender) {
+                               ForEach(genders, id: \.self) {
+                                   Text($0)
+                               }
+                           }.padding(10)
+                           TextField("CreditCard", text: $creditCard).padding(10)
+                       }
+                       Section {
+                           TextView(text: $bio, textStyle: $textStyle)
+                               .padding(.horizontal)
+                               
+                       }
+                       Section {
+                           Button("Register", action: {
+                               
+                               // TODO: проверки на заполнение полей и алерты
+                               // TODO: проверка что такой пользователь уже "существует" (доработка vapor)
+                               let idUser = Int.random(in: 1..<10000)
+                               AuthMethods().register(idUser: idUser, username: username, password: password, email: email, gender: selectedGender, creditCard: creditCard, bio: bio) { result, error in
+                                   guard let result else {
+                                       print(error?.localizedDescription)
+                                       return
+                                   }
+                                   print(result)
+                                   DispatchQueue.main.async { dismissAction() }
+                               }
+                           })
+                           Button("Dismiss", action: dismissAction)
+                       }
+                   }
+                   .navigationBarTitle("Register")
+               }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView {}
+    }
+}
+
+struct TextView: UIViewRepresentable {
+    
+    @Binding var text: String
+    @Binding var textStyle: UIFont.TextStyle
+ 
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: textStyle)
+        textView.autocapitalizationType = .sentences
+        textView.isSelectable = true
+        textView.isUserInteractionEnabled = true
+        textView.isScrollEnabled = false
+ 
+        return textView
+    }
+ 
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+        uiView.font = UIFont.preferredFont(forTextStyle: textStyle)
+    }
+}
--- EMag/EMag/App/Flows/ViewController.swift
+++ EMag/EMag/App/Flows/ViewController.swift
@@ -1,14 +1,73 @@
-//Created by chizztectep on 11.06.2023 
+// Created by chizztectep on 11.06.2023
 // v 1.0.0 lesson6
 import UIKit
+import SwiftUI
 
 // https:/ /github.com/GeekBrainsTutorial/online-store-api
+// https://stackoverflow.com/questions/57190511/dismiss-a-swiftui-view-that-is-contained-in-a-uihostingcontroller
 
 class ViewController: UIViewController {
+    
+    private var registerView: UIHostingController<RegisterView>?
+    
+    private var user: LoginResult?
+    
+    // MARK: - Outlets
+    @IBOutlet private weak var loginTextInput: UITextField!
+    @IBOutlet weak var passwordTextInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        // сделать запрос на авторизацию
+        let authController = AuthMethods()
+        authController.login(username: loginTextInput.text!, password: passwordTextInput.text!) { result in
+            guard let result  else {
+                // выводим сообщение о том, что не получилось зайти
+                print("error login")
+                return
+            }
+            // переходим на экран с данными пользователя
+            print("ok login: \(result.user.id)")
+            self.user = result
+            DispatchQueue.main.async { self.performSegue(withIdentifier: "successLogin", sender: self)
+            }
+        }
+    }
+    
+
+    
+    @IBAction func registerButtonAction(_ sender: Any) {
+        present(registerView!, animated: true)
+//        addChild(registerView)
+//        view.addSubview(registerView.view)
+//        registerView.didMove(toParent: self)
+//
+//        registerView.view.translatesAutoresizingMaskIntoConstraints = false
+//
+//        [registerView.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//         registerView.view.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//         registerView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//         registerView.view.topAnchor.constraint(equalTo: view.topAnchor),
+//         registerView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)].forEach { $0.isActive = true }
+        
+    }
+    
     private var userId: Int = 0
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         guard segue.identifier == "successLogin" else { return }
+         guard let destination = segue.destination as? UserViewController else { return }
+        destination.user = self.user
+     }
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       registerView = UIHostingController(rootView: RegisterView(dismissAction: {
+            self.dismiss(animated: true, completion: nil)
+        }))
         // Do any additional setup after loading the view.
         let auth = RequestFactory.shared.makeAuthRequestFatory()
           // test login
@@ -23,6 +82,8 @@ class ViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
+        
+        
          // test logout
          auth.logout(idUser: self.userId) { response in
                 switch response.result {
